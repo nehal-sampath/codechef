e3502abb817a8d96fb2333c32440d887339b429b
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-02-21 21:58:27 +0530

    Create Problem(Farm animals) - Generate examples.py

---
+++ 4.logic_building/Problem(Farm animals) - Generate examples.py
@@ -0,0 +1,21 @@
+# Update the '_' in the code below to solve this problem
+
+X = 3
+Y = 4
+Z = 12
+print('The animal on the farm is', 'ANY')
+
+X = 3
+Y = 5
+Z = 13
+print('The animal on the farm is', 'NONE')
+
+X = 3
+Y = 5
+Z = 10
+print('The animal on the farm is', 'DUCK')
+
+X = 3
+Y = 5
+Z = 9
+print('The animal on the farm is', 'CHICKEN')
