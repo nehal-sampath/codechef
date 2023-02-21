bee8385c7c155250dcb2e94a1473c578f30d09be
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-02-21 22:02:44 +0530

    Create Problem (MOVIE2X) - Generate examples.py

---
+++ 4.logic_building/Problem (MOVIE2X) - Generate examples.py
@@ -0,0 +1,17 @@
+# Update the '_' in the code below and click 'Submit'
+
+X = 90
+Y = 40
+print('Given X and Y, the total watchtime of the movie is _')
+
+X = 80
+Y = 80
+print('Given X and Y, the total watchtime of the movie is _')
+
+X = 60
+Y = 10
+print('Given X and Y, the total watchtime of the movie is _')
+
+X = 60
+Y = 50
+print('Given X and Y, the total watchtime of the movie is _')
