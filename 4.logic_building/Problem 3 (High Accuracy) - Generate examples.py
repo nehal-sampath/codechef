2694aa547e9ff61ba7e850a6031fe9f4c525ab38
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-02-21 22:00:37 +0530

    Create Problem 3 (High Accuracy) - Generate examples.py

---
+++ 4.logic_building/Problem 3 (High Accuracy) - Generate examples.py
@@ -0,0 +1,13 @@
+# Update the blanks in the statements below
+
+X = 18
+print('minimum number of problems Chef marked incorrect are 0')
+
+X = 19
+print('minimum number of problems Chef marked incorrect are 2')
+
+X = 20
+print('minimum number of problems Chef marked incorrect are 1')
+
+X = 7
+print('minimum number of problems Chef marked incorrect are 2')
