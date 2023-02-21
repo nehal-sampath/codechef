4b912f70088dbd6db46e2722968e57318c505e67
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-02-21 21:53:11 +0530

    Create Problem (Make Avg) - Generate examples.py

---
+++ 4.logic_building/Problem (Make Avg) - Generate examples.py
@@ -0,0 +1,9 @@
+# Update the '_' in the code below with the expected output
+
+print("For A = 3 and C = 4, the output is", -1)
+
+print("For A = 3 and C = 5, the output is", 4)
+
+print("For A = 2 and C = 4, the output is", 3)
+
+print("For A = 2 and C = 5, the output is", -1)
