501d9a634abfe060a955fa81b2a994c486993200
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-02-22 19:09:31 +0530

    Create Some useful math functions.py

---
+++ 1.Getting started on CodeChef.py/Some useful math functions.py
@@ -0,0 +1,24 @@
+#Click on 'Submit' to check the output of the functions
+#Click on 'Next' to proceed
+
+A = 30
+B = 45
+C = 36
+
+#Output the maximum value of A, B, C
+print(max(A, B, C))
+
+#Output the minimum value of A, B, C
+print(min(A, B, C))
+
+#Output the absolute value of the difference of A and C
+print(abs(A - C))
+
+#Output the absolute value of the difference of C and A
+print(abs(C - A))
+
+D = 6
+E = 9
+#Output the remainder when A is divided by D and E
+print(A % D)
+print(A % E)
