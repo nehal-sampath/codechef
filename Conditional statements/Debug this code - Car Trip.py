dc8c1931684a0d68392a0e318e5e0e1fed350684
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-02-22 18:13:05 +0530

    Create Debug this code - Car Trip.py

---
+++ Conditional statements/Debug this code - Car Trip.py
@@ -0,0 +1,10 @@
+# The code below is incorrect. Debug this code to solve the problem
+
+t=int(input())
+for j in range(t):
+    X=int(input())
+    if X<=300:               
+        print(3000)
+    else:
+        print(X*10)
+    
