4cc74b8067a30c07c16b15744613817b80289ffa
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-02-22 18:09:23 +0530

    Create Module on basic conditional statements.py

---
+++ Conditional statements/Module on basic conditional statements.py
@@ -0,0 +1,22 @@
+#Click on 'Submit' to first see the results
+#Click on 'Next to continue'
+
+# if, else statement and indentation
+a = 13
+b = 15
+if a >= b:
+    #do not forget the indentation!!!
+    print(a, 'is greater than or equal to', b)        
+else:
+    #do not forget the indentation!!!
+    print(a, 'is lesser than', b)
+
+# lets add the elif statement
+a = 14
+b = 14
+if a > b:
+    print(a, 'is greater than', b)
+elif a == b:
+    print(a, 'is equal to', b)
+else:
+    print(a, 'is lesser than', b)
