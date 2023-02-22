15a6617d2610967e11228d7459b1745d469e2cd9
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-02-22 18:11:13 +0530

    Create Practice problem - Alice and Marks.py

---
+++ Conditional statements/Practice problem - Alice and Marks.py
@@ -0,0 +1,12 @@
+# Update the code below to solve this problem
+
+#Note this line has been commented out there is no 't' in this problem
+#t = int(input())           
+
+#Accept 2 integers as input
+X,Y = map(int, input().split())          
+if(X>=2*Y):
+    print("Yes")
+else:
+    print("No")
+    
