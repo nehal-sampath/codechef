5f4e66671055cef6f001343484a411c000ad62ac
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-02-23 14:50:09 +0530

    Create Problem (Flip cards) - Generate examples.py

---
+++ 4.logic_building/Problem (Flip cards) - Generate examples.py
@@ -0,0 +1,17 @@
+# Update the '_' below to solve this problem
+
+N = 10
+X = 3
+print('The number of cards we need to flip is 3')
+#HERE logic is when x/2>y print(y) otherwise print(x-y)
+N = 10
+X = 7
+print('The number of cards we need to flip is 3')
+
+N = 10
+X = 4
+print('The number of cards we need to flip is 4')
+
+N = 10
+X = 6
+print('The number of cards we need to flip is 4')
