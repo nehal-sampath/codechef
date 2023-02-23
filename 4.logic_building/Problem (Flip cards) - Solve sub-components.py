5b109adac1a40f2cb812e790c46e4bb225d00aba
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-02-23 14:55:14 +0530

    Create Problem (Flip cards) - Solve sub-components.py

---
+++ 4.logic_building/Problem (Flip cards) - Solve sub-components.py
@@ -0,0 +1,16 @@
+#Update the '_' below to solve the problem
+
+N = 10
+X = 4
+option1 = X
+option2 = N-X
+print(min(option1, option2))
+
+
+N = 10
+X = 6
+option1 = X
+option2 = N-X
+print(min(option1, option2))
+ 
+
