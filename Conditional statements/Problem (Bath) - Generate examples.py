dedf38688d88f94179f02e1c09b0154182c7df37
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-02-25 21:42:01 +0530

    Create Problem (Bath) - Generate examples.py

---
+++ Conditional statements/Problem (Bath) - Generate examples.py
@@ -0,0 +1,17 @@
+# Update the '_' in the code below to solve the problem
+
+X = 15
+Y = 1
+print('The maximum number of people that can take a bath is 7')
+
+X = 15
+Y = 2
+print('The maximum number of people that can take a bath is 3')
+
+X = 15
+Y = 3
+print('The maximum number of people that can take a bath is 2')
+
+X = 15
+Y = 4
+print('The maximum number of people that can take a bath is 1')
