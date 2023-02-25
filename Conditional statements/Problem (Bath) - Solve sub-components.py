5ec0a515966b93fc147c49cc190c926a4814bc4a
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-02-25 21:45:11 +0530

    Create Problem (Bath) - Solve sub-components.py

---
+++ Conditional statements/Problem (Bath) - Solve sub-components.py
@@ -0,0 +1,14 @@
+# Update the '_' in the code below to proceed
+
+X = 15
+Y = 1
+#count of completely filled buckets
+buckets_filled = X//Y    
+people = buckets_filled//2
+print(people)
+
+X = 15
+Y = 3
+buckets_filled = X//Y       
+people = buckets_filled//2
+print(people)
