d7b4e4b68958d75c8ac97c4d214e644c8c969c5b
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-03-03 19:05:51 +0530

    Create Problem (Shoes) - Generate examples.py

---
+++ 4.logic_building/Problem (Shoes) - Generate examples.py
@@ -0,0 +1,13 @@
+# Update the '_' in the code below to solve this problem
+
+N = 10
+M = 10
+print('The number of extra shoes that Chef has to buy is 10')
+
+N = 5
+M = 10
+print('The number of extra shoes that Chef has to buy is 5')
+
+N = 15
+M = 10
+print('The number of extra shoes that Chef has to buy is 20')
