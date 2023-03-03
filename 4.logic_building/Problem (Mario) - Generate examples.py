66aa4a3f872818fdbc1adf7a530de85b13e1d444
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-03-03 19:18:04 +0530

    Create Problem (Mario) - Generate examples.py

---
+++ 4.logic_building/Problem (Mario) - Generate examples.py
@@ -0,0 +1,10 @@
+# Update the '_' in the code below to solve the problem
+
+X = 12
+print("Mario's size after eating",X,"mushrooms is NORMAL")
+
+X = 13
+print("Mario's size after eating",X,"mushrooms is HUGE")
+
+X = 14
+print("Mario's size after eating",X,"mushrooms is SMALL")
