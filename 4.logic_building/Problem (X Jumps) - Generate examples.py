fd6bfdcbcf1f5c19e42293e53412449037764a0f
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-03-03 19:38:16 +0530

    Create Problem (X Jumps) - Generate examples.py

---
+++ 4.logic_building/Problem (X Jumps) - Generate examples.py
@@ -0,0 +1,13 @@
+# Update the '_' in the code below to solve the problem
+
+X = 18
+Y = 3
+print("Minimum moves required to reach stair",X,"is 6")
+
+X = 19
+Y = 3
+print("Minimum moves required to reach stair",X,"is 7")
+
+X = 2
+Y = 3
+print("Minimum moves required to reach stair",X,"is 2")
