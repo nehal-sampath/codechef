b6a9deb95aa545339be351256baa25dbf802bdaa
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-03-03 19:44:07 +0530

    Create Problem (X Jumps) - Solve sub-components.py

---
+++ 4.logic_building/Problem (X Jumps) - Solve sub-components.py
@@ -0,0 +1,15 @@
+# Update the '_' in the code below to solve this problem
+
+X = 19
+Y = 3
+if X%Y == 0:            
+    print("Count of moves is",X//Y)
+else:
+    print("Count of moves is",(X//Y) + (X%Y))
+
+X = 20
+Y = 3
+if X%Y == 0:            
+    print("Count of moves is",X//Y)
+else:
+    print("Count of moves is",(X//Y) + (X%Y))
