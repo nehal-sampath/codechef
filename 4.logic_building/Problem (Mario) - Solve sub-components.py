761705edad59d4dda4a98c2a0c9e838b3cf3dc10
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-03-03 19:24:44 +0530

    Create Problem (Mario) - Solve sub-components.py

---
+++ 4.logic_building/Problem (Mario) - Solve sub-components.py
@@ -0,0 +1,17 @@
+# Update the '_' in the code below to solve the problem
+
+X = 12
+if X%3==0:            
+    print("NORMAL")
+elif (X-1)%3 == 0:
+    print("HUGE")
+else:
+    print("SMALL")
+
+X = 13
+if X%3 == 0:            
+    print("NORMAL")
+elif (X-1)%3 == 0:
+    print("HUGE")
+else:
+    print("SMALL")
