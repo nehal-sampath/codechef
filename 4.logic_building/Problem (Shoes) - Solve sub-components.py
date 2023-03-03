f87337c8e11cccd3f8001ced799e58962a1d5748
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-03-03 19:11:23 +0530

    Create Problem (Shoes) - Solve sub-components.py

---
+++ 4.logic_building/Problem (Shoes) - Solve sub-components.py
@@ -0,0 +1,17 @@
+# Update the '_' in the code below to solve this problem
+
+N = 10
+M = 10
+if N <= M:
+    print('We need to buy',N,'shoes')
+else:
+    print('We need to buy',(2*N-M),'shoes')
+
+
+N = 15
+M = 10
+if N <= M:
+    print('We need to buy',N,'shoes')
+else:
+    print('We need to buy',(2*N-M),'shoes')
+
