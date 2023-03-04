8c2dfecb8172a8bec9daf528b52516bb3678c0c7
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-03-04 17:42:51 +0530

    Create Practice problem - Minimum number of coins.py

---
+++ 4.logic_building/Practice problem - Minimum number of coins.py
@@ -0,0 +1,11 @@
+# Update the code below to solve the problem
+
+t = int(input())
+for i in range(t):
+    X = int(input())
+    if(X%10==0):
+        print(X//10)
+    elif(X%5==0):
+        print((X//10)+1)
+    else:
+        print(-1)
