7c9e9f1c7844708e478d7f1ab65ba8623d0d7804
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-03-05 18:41:19 +0530

    Create Concatenation.cpp

---
+++ c++.cpp/Concatenation.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main () {
+  string x = "Hello";
+  string y = "World";
+  cout << x + " " + y ;     //remember to include a blank space between HellWorld
+  return 0;
+}
