eef8b77c9619ff664601a573139fd3693918649c
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-03-19 16:50:21 +0530

    Create String Operations.cpp

---
+++ c++.cpp/String Operations.cpp
@@ -0,0 +1,13 @@
+// Update the blanks below to print the first 3 characters of the String
+// There is also a syntax error - try and fix it to debug the code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string var = "String";
+  cout << var[ 0 ];     //Print the first 3 characters - their index is 0, 1, 2
+  cout << var[ 1 ];
+  cout << var[ 2 ];
+  return 0;             //The ; was missing. This is a very common mistake!!!    
+}
