ea554e96e53dee2851a8bf1339e51f6b193ec016
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-03-20 17:53:20 +0530

    Create Find the Area of any rectangle.cpp

---
+++ c++.cpp/Find the Area of any rectangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int length, width ;
+  int area ;
+  cin >> length ;
+  cin >> width ;
+  area = length * width ;
+  cout << "Area of the rectangle is: " <<area ;
+  return 0;
+}
