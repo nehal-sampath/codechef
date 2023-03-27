d7775083d78ddcd3e76659cc94ed27426120c396
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-03-27 18:51:26 +0530

    Create And Statement.cpp

---
+++ c++.cpp/And Statement.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a;
+  int b;
+  a = 15;
+  b = 35;
+  if (a%7 == 0 && a%5 == 0 ) {
+    cout << "The number is divisible by both 5 & 7" << endl;
+  }  
+  else {
+    cout << "The number is not divisible by both 5 & 7" << endl;
+  }
+
+  if (b%7 == 0 && b%5 == 0 ) {
+    cout << "The number is divisible by both 5 & 7" << endl;
+  } 
+  else {
+    cout << "The number is not divisible by both 5 & 7" << endl;
+  }
+  return 0;
+}
