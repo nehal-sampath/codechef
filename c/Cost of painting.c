ce57be532f82812ce638ed24ac2bc60f72dbfc7c
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-04-13 16:22:53 +0530

    Create Cost of painting.c

---
+++ c/Cost of painting.c
@@ -0,0 +1,13 @@
+//Update the blanks in the code below to solve the problem
+
+#include <stdio.h>
+
+int main() {
+  int s = 14 ;
+  int area = s * s ;
+  int cost = area * 7 ;
+  printf("%d \n", area );   //Area and cost have to be output on separate lines
+  printf("%d", cost );      //'$' has to be printed after cost without any space
+  printf("$"); 
+  return 0;
+}
