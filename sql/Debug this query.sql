bdc0d87a95436d7d5728df5309275a5157e000d6
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-05-01 05:51:31 +0530

    Create Debug this query.sql

---
+++ sql/Debug this query.sql
@@ -0,0 +1,9 @@
+/* The Query written in the console is trying to insert data to the table employee.
+Debug this query to output the entire table */
+
+INSERT INTO employee(Id,Name,Age,Address)
+VALUES  (1, 'John Smith', 25,  '123 Main St'),
+        (2, 'Sarah Johnson', 30,'456 Broadway');   
+#here email is not mentioned in the column names
+
+SELECT * FROM employee; 
