3929c78158dd2bf2fa5699b1001a659737b2069a
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-05-01 05:44:42 +0530

    Create Insert table.sql

---
+++ sql/Insert table.sql
@@ -0,0 +1,5 @@
+/* Write a query to insert the below mentioned employee details to the table 'employee' */
+
+INSERT INTO employee(Employee_id,Employee_Name,Department)
+VALUES (04,'Marcus Garcia','Product'),
+       (05,'Mike Tyson','Hr');
