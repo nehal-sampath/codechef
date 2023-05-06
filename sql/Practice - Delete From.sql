4a85c1fff82ac51da1e80ee031c7127f87095762
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-05-06 06:27:23 +0530

    Create Practice - Delete From.sql

---
+++ sql/Practice - Delete From.sql
@@ -0,0 +1,7 @@
+/* Write a query which does the following
+- Delete the row where the department is Client.
+- Output the entire table.
+*/
+DELETE FROM employee
+WHERE Department ='Client';   
+select * from employee;
