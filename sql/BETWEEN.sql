e4e7653d37e136da870cb9c78595aa2d870795ed
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-05-08 08:42:38 +0530

    Create BETWEEN.sql

---
+++ sql/BETWEEN.sql
@@ -0,0 +1,6 @@
+/* Write a query to do the following
+- Output all entries from from the table
+- Where passenger_name between 'A' and 'D' */
+
+select * from Flights
+where passenger_name BETWEEN 'A' AND 'D';
