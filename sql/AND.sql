1fe56b18a34628b9548118fef9dceb84be441d6b
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-05-09 06:51:31 +0530

    Create AND.sql

---
+++ sql/AND.sql
@@ -0,0 +1,6 @@
+/* write a query below that returns the rows which meet the following conditions
+- Gender - Female
+- Destination - Cairo */
+SELECT * FROM Flights
+WHERE destination = 'Cairo'
+AND gender =  'Female';
