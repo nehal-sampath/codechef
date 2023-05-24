fe2192c70144ef8aca718b793a7566d8b92e0c32
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-05-24 06:01:21 +0530

    Create Input mirror.c

---
+++ c/C for problem solving - 1/Input mirror.c
@@ -0,0 +1,13 @@
+//Update the _ in the IDE 
+#include <stdio.h>
+
+int main() {
+    int N;
+    char  S[10];
+    scanf("%d", &N);
+    scanf("%s", S);
+    printf("%d ", N );  
+    printf("%s ", S );  
+
+    return 0;
+}
