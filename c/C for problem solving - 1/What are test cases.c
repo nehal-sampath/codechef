e9f603b3cd5362afbb4f83e6b30e18434febbbaa
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-05-25 05:05:14 +0530

    Create What are test cases.c

---
+++ c/C for problem solving - 1/What are test cases.c
@@ -0,0 +1,19 @@
+//Update the _ in the code
+
+#include <stdio.h>
+
+int main() 
+{
+    int A, B, C, D, E;
+    scanf("%d \n", &A );
+    scanf("%d \n", &B );
+    scanf("%d \n", &C );
+    scanf("%d \n", &D );
+    scanf("%d \n", &E );
+    printf("%d \n", A );  
+    printf("%d \n", B );  
+    printf("%d \n", C );  
+    printf("%d \n", D );  
+    printf("%d \n", E );  
+    return 0;
+}
