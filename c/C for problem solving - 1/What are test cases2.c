b2c55af599b9f1e5d17759c52e943aeb3a917bf9
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-05-25 05:07:45 +0530

    Create What are test cases2.c

---
+++ c/C for problem solving - 1/What are test cases2.c
@@ -0,0 +1,16 @@
+//Update the '_' in the problem below to solve it
+
+#include <stdio.h>
+
+int main() 
+{
+    int t,n;
+    int i = 1;
+    scanf("%d", &t );
+    while ( i <= t) {
+        scanf("%d", &n );
+        printf("%d \n", n );
+        i = i+1;
+    }
+    return 0;
+}
