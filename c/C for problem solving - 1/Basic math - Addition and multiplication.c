c4629f14af97929ba11b27b1100b83e082850270
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-05-30 12:04:25 +0530

    Create Basic math - Addition and multiplication.c

---
+++ c/C for problem solving - 1/Basic math - Addition and multiplication.c
@@ -0,0 +1,18 @@
+// Update the code below to solve the problem
+
+#include <stdio.h>
+
+int main() {
+    int t,A,B;
+    int P,S;
+    int i = 1;
+    scanf("%d", &t );
+    while (i <= t){
+        scanf("%d %d", &A,&B);
+        S = A + B;
+        P = A * B;
+        printf("%d %d \n", S,P );
+        i = i+1;
+    }
+    return 0;
+}
