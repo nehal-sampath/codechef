3d5e58169d2fcb2cdf921ac198dcef7b6af63ee4
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-05-31 13:14:48 +0530

    Create Practice problem - Fitness.c

---
+++ c/C for problem solving - 1/Practice problem - Fitness.c
@@ -0,0 +1,16 @@
+// Complete the Code
+
+#include <stdio.h>
+
+int main() {
+    int t,X;
+    int i = 1;
+    scanf("%d", &t );
+    while (i <= t){
+        scanf("%d", &X);
+        printf("%d\n", (2*X)*5);
+        i = i+1;
+    }
+    return 0;
+}
+ 
