2d49e38b504676c3795b1df1aec586bca67ebb0c
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-06-01 18:02:34 +0530

    Create Subtraction and division of numbers.c

---
+++ c/C for problem solving - 1/Subtraction and division of numbers.c
@@ -0,0 +1,18 @@
+// Update the _ in the code
+
+#include <stdio.h>
+
+int main() {
+    int t,A,B;
+    int i = 1;
+    int D,Q;
+    scanf("%d", &t );
+    while (i <= t){
+        scanf("%d%d",&A,&B);
+        D = A - B;
+        Q = A / B;
+        printf("%d %d\n", D , Q);
+        i = i+1;
+    }
+    return 0;
+}
