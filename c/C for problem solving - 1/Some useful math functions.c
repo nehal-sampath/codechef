6f3cc60c6ae783a81cd75bd1debdbae334da3db1
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-06-04 06:52:37 +0530

    Create Some useful math functions.c

---
+++ c/C for problem solving - 1/Some useful math functions.c
@@ -0,0 +1,30 @@
+//Click on 'Submit' to check the output of the functions
+//Click on 'Next' to proceed
+//note \n is used to take the cursor to the next line
+
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+    double X=53.56;
+    double Y=3;
+    int A = 45;
+    int B = 20;
+   
+    // Output the absolute value of the difference of A and B
+    printf("%d\n", abs(A - B));
+
+    // Output the remainder when A is divided by B
+    printf("%d\n", A % B);
+
+    //computing the rounded off number upwards to its nearest integer and printing it
+    printf("%f\n", ceil(X));
+   
+    //computing the rounded off number downwards to its nearest integer and printing it
+    printf("%f\n", floor(X));
+
+    //computing the value of X to the power of Y and printing it
+    printf("%f\n", pow(X,Y));
+
+    return 0;
+}
