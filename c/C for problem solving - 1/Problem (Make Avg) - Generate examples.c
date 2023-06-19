12d4b95773be81b956ba6993c8076fd46100106a
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-06-19 18:09:00 +0530

    Create Problem (Make Avg) - Generate examples.c

---
+++ c/C for problem solving - 1/Problem (Make Avg) - Generate examples.c
@@ -0,0 +1,11 @@
+// Update the '_' in the code below with the expected output
+
+#include <stdio.h>
+
+int main() {
+    printf("For A = 3 and C = 4, the output is %d\n", -1);
+    printf("For A = 3 and C = 5, the output is %d\n", 4);
+    printf("For A = 2 and C = 4, the output is %d\n", 3);
+    printf("For A = 2 and C = 5, the output is %d\n", -1);
+    return 0;
+}
