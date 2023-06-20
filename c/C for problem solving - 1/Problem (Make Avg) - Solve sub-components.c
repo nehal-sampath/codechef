36ab75851041ac630aa691e6d149be95e9942369
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-06-20 06:16:37 +0530

    Create Problem (Make Avg) - Solve sub-components.c

---
+++ c/C for problem solving - 1/Problem (Make Avg) - Solve sub-components.c
@@ -0,0 +1,17 @@
+// How to find the average of 2 numbers? 
+
+#include <stdio.h>
+
+int main() {
+    int A = 15;
+    int C = 19;
+    int B = (A + C)/2;
+    printf("%d\n", B);
+    
+    A = 16;
+    C = 18;
+    B = (A + C)/2;
+    printf("%d\n", B);
+    
+    return 0;
+}
