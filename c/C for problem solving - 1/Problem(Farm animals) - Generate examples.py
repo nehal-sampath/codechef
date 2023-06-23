63f5279cb6400861e5efdecd092574829bfe7764
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-06-23 14:04:39 +0530

    Create Problem(Farm animals) - Generate examples.py

---
+++ c/C for problem solving - 1/Problem(Farm animals) - Generate examples.py
@@ -0,0 +1,28 @@
+// Update the '_' in the code below to solve this problem
+
+#include <stdio.h>
+
+int main() 
+{
+    int X = 3;
+    int Y = 4;
+    int Z = 12;
+    printf("The animal on the farm is ANY\n");
+
+    X = 3;
+    Y = 5;
+    Z = 13;
+    printf("The animal on the farm is NONE\n");
+
+    X = 3;
+    Y = 5;
+    Z = 10;
+    printf("The animal on the farm is DUCK\n");
+
+    X = 3;
+    Y = 5;
+    Z = 9;
+    printf("The animal on the farm is CHICKEN\n");
+
+    return 0;
+}
