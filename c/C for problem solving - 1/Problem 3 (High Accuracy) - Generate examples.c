4a281abe7afd61a4822b8a61ac159f1916d18829
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-06-26 09:07:23 +0530

    Create Problem 3 (High Accuracy) - Generate examples.c

---
+++ c/C for problem solving - 1/Problem 3 (High Accuracy) - Generate examples.c
@@ -0,0 +1,19 @@
+// Update the blanks in the statements below
+
+#include <stdio.h>
+
+int main() {
+    int X = 18;
+    printf("minimum number of problems Chef marked incorrect are 0\n");
+    
+    X = 19;
+    printf("minimum number of problems Chef marked incorrect are 2\n");
+
+    X = 20;
+    printf("minimum number of problems Chef marked incorrect are 1\n");
+
+    X = 7;
+    printf("minimum number of problems Chef marked incorrect are 2\n");
+
+    return 0;
+}
