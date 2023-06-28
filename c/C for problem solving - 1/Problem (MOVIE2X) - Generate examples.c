5a6b18f1be8d19fcf5f269e53a49ec1e8057be10
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-06-28 17:48:06 +0530

    Create Problem (MOVIE2X) - Generate examples.c

---
+++ c/C for problem solving - 1/Problem (MOVIE2X) - Generate examples.c
@@ -0,0 +1,24 @@
+// Update the '_' in the code below and click 'Submit'
+
+#include <stdio.h>
+
+int main() 
+{
+    int X = 90;
+    int Y = 40;
+    printf("Given X and Y, the total watchtime of the movie is 70\n");
+
+    X = 80;
+    Y = 80;
+    printf("Given X and Y, the total watchtime of the movie is 40\n");
+
+    X = 60;
+    Y = 10;
+    printf("Given X and Y, the total watchtime of the movie is 55\n");
+
+    X = 60;
+    Y = 50;
+    printf("Given X and Y, the total watchtime of the movie is 35\n");
+
+    return 0;
+}
