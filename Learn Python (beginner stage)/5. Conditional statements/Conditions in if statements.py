0ada8865516a2ef353f4384472d7cfe455f13123
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-07-07 09:35:30 +0530

    Create Conditions in if statements.py

---
+++ Learn Python (beginner stage)/5. Conditional statements/Conditions in if statements.py
@@ -0,0 +1,20 @@
+a, c = map(int, input().split())      # Will cover this syntax for accepting multiple inputs later
+
+# Update the code below this line to complete Task 1
+if a>=c:
+    print("Bravo!")
+else:
+    print("Try Again")
+    
+
+
+
+
+
+a, c = map(int, input().split())     # Will cover this syntax for accepting multiple inputs later
+if a>=c:
+    print("Bravo!")
+else:
+    print("Try Again")
+
+# Update the code below this line to complete Task 2
