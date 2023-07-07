76336a592142d47cc511732b8c7d51fe032dd675
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-07-07 10:08:52 +0530

    Create Problems on Conditional Statements.py

---
+++ Learn Python (beginner stage)/5. Conditional statements/Problems on Conditional Statements.py
@@ -0,0 +1,16 @@
+x = int(input())
+# Update your code below this line for 1st user input
+if(x<70):
+    print("Order Confirmed")
+else:
+    print("Order Limit reached")
+print("Thank YOU!")
+x = int(input())
+# Update your code below this line for 2nd user input
+if(x<70):
+    print("Order Confirmed")
+else:
+    print("Order Limit reached")
+print("Thank YOU!")
+
+
