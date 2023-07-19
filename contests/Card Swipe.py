201e064443fa1b4f3fc5fe97a6225689080373c1
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-07-19 21:58:52 +0530

    Update Card Swipe.py

--- contests/Card Swipe.py
+++ contests/Card Swipe.py
@@ -1,17 +1,1 @@
-T = int(input())
 
-for _ in range(T):
-    N = int(input())
-    swipes = list(map(int, input().split()))
-
-    count = 0  
-    max_count = 0  
-
-    for swipe in swipes:
-        if swipe % 2 == 1:
-            count += 1
-        else:
-            count -= 1
-        max_count = max(max_count, count)
-
-    print(max_count)
