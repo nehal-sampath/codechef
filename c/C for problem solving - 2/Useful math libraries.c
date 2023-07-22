a9c0a005b2fe7c6bad0b656f3dd071e137ac8c39
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-07-22 22:09:05 +0530

    Create Useful math libraries.c

---
+++ c/C for problem solving - 2/Useful math libraries.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    double n = 26;
+    printf("%lf\n", sqrt(n));
+    
+    double f = 1.89;
+    printf("%lf\n", floor(f));
+    
+    n = 0.21;
+    printf("%lf\n", ceil(n));
+    
+    int x = 6;
+    int y = 3;
+    printf("%lf\n", pow(x,y));
+
+    return 0;
+}
