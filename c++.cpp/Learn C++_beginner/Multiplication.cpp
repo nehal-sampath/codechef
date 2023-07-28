055af23458f067f947705040d0ee401db6245c05
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-07-28 17:57:02 +0530

    Create Multiplication.cpp

---
+++ c++.cpp/Learn C++_beginner/Multiplication.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b,c,d,result1,result2;
+    a = 8;
+    b = 9;
+    c = 100;
+    d = 200;
+    result1 = a*b;
+    result2 = (c+d)*2;
+    cout<<result1<<endl;
+    cout<<result2<<endl;
+    
+}
