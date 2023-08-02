cccfe93061845160c14451b5a599f02058bbf796
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-08-02 06:56:26 +0530

    Create Area of Rectangle.cpp

---
+++ c++.cpp/Learn C++_beginner/Area of Rectangle.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  int length = 45;
+  int width = 76;
+  int area = length * width;
+  cout << "The Area of the given rectangle is ";
+  cout << area; 
+
+  return 0;
+}
