1de5fe45c2762035f7a758d1feb494bd9b734573
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-08-05 06:23:43 +0530

    Create Cost of painting.cpp

---
+++ c++.cpp/Learn C++_beginner/Cost of painting.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int s = 14;
+  int area = s * s;
+  int cost = area * 7;
+  cout << area << endl;     //area and cost have to be output on separate lines
+  cout << cost;             //output the $ character after cost without any space
+  cout << "$";
+  
+  return 0;
+}
