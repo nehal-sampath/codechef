a59f7507aeb89cf87adcf357a9b0421b6e0331f7
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-08-09 17:52:39 +0530

    Create String Operations.cpp

---
+++ c++.cpp/Learn C++_beginner/String Operations.cpp
@@ -0,0 +1,15 @@
+// Update the blanks below to print the first 3 characters of the String
+// There is also a syntax error - try and fix it to debug the code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  string var = "String";
+  cout << var[ 0 ];
+  cout << var[ 1 ];
+  cout << var[ 2 ];
+
+  return 0;
+}
