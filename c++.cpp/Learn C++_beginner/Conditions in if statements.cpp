32026574d0f2990c4e768e72c19e9fb9a180b7f2
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-08-15 05:21:31 +0530

    Create Conditions in if statements.cpp

---
+++ c++.cpp/Learn C++_beginner/Conditions in if statements.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  int a;
+  int b;
+  int c;
+  cin >> a;
+  cin >> b;
+  cin >> c;
+  if (a >= c) {
+    cout << "Bravo!" << endl;
+  } 
+  else {
+    cout << "Try Again" << endl;
+  }
+
+  cin >> a;
+  cin >> b;
+  cin >> c;
+  if (a >= c) {
+    cout << "Bravo!";
+  } 
+  else {
+    cout << "Try Again";
+  }
+
+  return 0;
+}
