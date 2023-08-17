ab0ac15d183beb777e980e64af110585a07a4757
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-08-17 08:02:11 +0530

    Create Or Statement.cpp

---
+++ c++.cpp/Learn C++_beginner/Or Statement.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  int z;
+  int x;
+  int c;
+  cin >> z;
+  cin >> x;
+  cin >> c;
+  if (c > x && c > z ) {
+    cout << "PASS" << endl;
+  } 
+  else {
+    cout << "FAIL" << endl;
+  }
+  
+  cin >> z;
+  cin >> x;
+  cin >> c;
+  if (c > x && c > z ) {
+    cout << "PASS";
+  } 
+  else {
+    cout << "FAIL";
+  }
+
+  return 0;
+}
