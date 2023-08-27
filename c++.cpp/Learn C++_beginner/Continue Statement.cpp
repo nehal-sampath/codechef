f8ad966e836538ce670d0487b882526c1f79919c
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-08-27 10:52:59 +0530

    Create Continue Statement.cpp

---
+++ c++.cpp/Learn C++_beginner/Continue Statement.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  int n;
+  cin >> n;
+  for(int i = 0; i <= 12; i = i + 1) {
+    if (i == n) {
+      continue;
+    }
+    cout << i << endl;
+  }   
+
+  return 0;
+}
+
+
