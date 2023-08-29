7fca98639f0c356ec67d245be17eb4df1dbd7a6d
nehal-sampath <126001021+nehal-sampath@users.noreply.github.com>
2023-08-29 10:34:20 +0530

    Create For Each Loop.cpp

---
+++ c++.cpp/Learn C++_beginner/For Each Loop.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  int num[8] = {10, 20, 30, 40, 50, 60, 70, 80};
+  for(int i : num) {
+   cout << i << endl;
+  }
+
+  return 0;
+} 
